aa2e3b24da38fc613fcb0bf25ded5afcd9bb8a74
arthur voryshev <what_to_write@mail.ru>
2025-01-30 19:33:06 +1000

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,26 @@ import {Typography, Box, Container} from '@mui/material';
 
 import s from './Home.module.scss';
 
-import Building from '@/components/pages/home/Building';
+// import Building from '@/components/pages/home/Building';
 
 export default function Home() {
   return (
     <div className={s.home}>
       <Container className={s.home__text}>
         <Box>
-          <Typography variant="h3" component="h1">
+          <Typography className={s.main__text} variant="h3" component="h1">
             HeroMaker
           </Typography>
           <Typography className={s.text}>
             Добро пожаловать в HeroMaker — инструмент создания уникальных
-            персонажей для игр Dungeons & Dragons. Настройте своего героя до
-            мельчайших деталей, выбирая расы, классы и предысторию. Выгружайте
-            готовый лист персонажа в PDF или играйте онлайн с интерактивным
-            листом!
+            персонажей для игр Dungeons & Dragons <br />
+            Настройте своего героя до мельчайших деталей, выбирая расы, классы и
+            предысторию <br />
+            Выгружайте готовый лист персонажа в PDF или играйте онлайн с
+            интерактивным листом
           </Typography>
           <Typography className={s.text}>
-            Создавай. Играй. Вдохновляйся!
+            Создавай! Играй! Вдохновляйся!
           </Typography>
         </Box>
       </Container>
