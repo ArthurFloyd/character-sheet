5c1d9d9eaf92db0b6b0cc2b6260a3ec1ee025b25
arthur voryshev <what_to_write@mail.ru>
2025-02-13 18:59:16 +1000

    create character header

--- src/app/builder/layout.tsx
+++ src/app/builder/layout.tsx
@@ -1,10 +1,13 @@
 import {ReactNode} from 'react';
+
 import Builder from './page';
+import CharacterHeader from '@/components/shared/Character Header/CharacterHeader';
 
 export default function Layout({children}: {children: ReactNode}) {
   return (
     <div>
       <Builder />
+      <CharacterHeader />
       <main>{children}</main>
     </div>
   );
